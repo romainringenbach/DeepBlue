20c77df2e0f171191530c544a0ed43f84dcc6c48
nealith <nealith@neaworld.fr>
2019-01-25 23:25:04 +0100

    begin cleaning godot project

---
+++ cockpit_exp/speed_controller/speed_controller.gd
@@ -0,0 +1,39 @@
+extends Spatial
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+var left_click_pressed = false
+var last_position
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+func change_speed(delta):
+	if $Cylinder_001.rotation_degrees.x  + delta.y >= -15 and $Cylinder_001.rotation_degrees.x  + delta.y <= 78:
+		$Cylinder_001.rotate_x(delta.y * 0.01)
+	
+
+
+func _on_StaticBody_input_event(camera, event, click_position, click_normal, shape_idx):
+	if event is InputEventMouseButton:
+		if event.pressed and event.button_index == 1:
+			last_position = event.position
+			left_click_pressed = true
+		elif event.pressed == false and event.button_index == 1:
+			left_click_pressed = false
+	elif event is InputEventMouseMotion and left_click_pressed:
+		change_speed(event.position - last_position)
+		last_position = event.position
+		
+
+func _on_StaticBody_mouse_exited():
+	left_click_pressed = false
