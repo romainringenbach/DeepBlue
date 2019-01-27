3bacfae289f64a7b1819d3320673346fa271347b
nealith <nealith@neaworld.fr>
2019-01-27 15:31:18 +0100

    improve camera by adding zoom and limiting avaiable rotations

--- cockpit_exp/cockpit/InterpolatedCamera.gd
+++ cockpit_exp/cockpit/InterpolatedCamera.gd
@@ -2,30 +2,47 @@ extends InterpolatedCamera
 
 onready var Yaw = get_parent()
 
+export (float) var MAX_ZOOM = 20 
+export (float) var MAX_UNZOOM = 55
+export (float) var pas = 5
+
 func _ready():
 	## Tell Godot that we want to handle input
 	#set_process_input(true)
 	pass
-
+	
 func look_updown_rotation(rotation = 0):
 	"""
 	Get the new rotation for looking up and down
 	"""
-	var toReturn = self.get_rotation() + Vector3(rotation, 0, 0)
+	var r_rotation = self.get_rotation() + Vector3(rotation, 0, 0)
+	if r_rotation.x < -90:
+		r_rotation.x = -90
+	if r_rotation.x > 90:
+		r_rotation.x = 90
 
 	##
 	## We don't want the player to be able to bend over backwards
 	## neither to be able to look under their arse.
 	## Here we'll clamp the vertical look to 90° up and down
-	toReturn.x = clamp(toReturn.x, PI / -2, PI / 2)
+	r_rotation.x = clamp(r_rotation.x, PI / -2, PI / 2)
 
-	return toReturn
+	return r_rotation
 
 func look_leftright_rotation(rotation = 0):
 	"""
 	Get the new rotation for looking left and right
 	"""
-	return Yaw.get_rotation() + Vector3(0, rotation, 0)
+	var r_rotation = Yaw.get_rotation() + Vector3(0, rotation, 0)
+	
+	if r_rotation.y < PI / -2:
+		r_rotation.y = PI / -2
+		print("merde")
+	if r_rotation.y > PI / 2:
+		r_rotation.y = PI / 2
+		print("merde2")
+	
+	return r_rotation
 
 func mouse(event):
 	"""
@@ -47,6 +64,15 @@ func _input(event):
 	## We'll only process mouse motion events
 	if event is InputEventMouseMotion and Input.is_action_pressed("right_click"):
 		return mouse(event)
+		
+	if Input.is_action_pressed("zoom"):
+		fov -= pas
+		if fov < MAX_ZOOM:
+			fov = MAX_ZOOM
+	if Input.is_action_pressed("unzoom"):
+		fov += pas
+		if fov > MAX_UNZOOM:
+			fov = MAX_UNZOOM
 
 func _enter_tree():
 	"""
