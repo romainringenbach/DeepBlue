0ac193fa6ff5081280d5321ce02c560e5d88c49b
nealith <nealith@neaworld.fr>
2019-01-27 14:03:14 +0100

    correct main controller animation

--- cockpit_exp/main_controller/main_controller.gd
+++ cockpit_exp/main_controller/main_controller.gd
@@ -4,10 +4,17 @@ extends Spatial
 # var a = 2
 # var b = "textvar"
 
-var up = false
-var down = false
-var left = false
-var right = false
+var animation_done_status = {
+	"up" : false,
+	"r_up" : true,
+	"down" : false,
+	"r_down" : true,
+	"left" : false,
+	"r_left" : true,
+	"right" : false,
+	"r_right" : true
+}
+
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -16,35 +23,34 @@ func _ready():
 
 func _process(delta):
 	
-	if Input.is_action_pressed("ui_up") and up == false:
-		$AnimationPlayer.play("up")
-		up = true
+	_anim(Input.is_action_pressed("ui_up"),"up","down",$AnimationPlayer)
+	_anim(Input.is_action_pressed("ui_down"),"down","up",$AnimationPlayer)
+	_anim(Input.is_action_pressed("ui_left"),"left","right",$AnimationPlayer2)
+	_anim(Input.is_action_pressed("ui_right"),"right","left",$AnimationPlayer2)
 	
-	if not Input.is_action_pressed("ui_up") and up == true:
-		$AnimationPlayer.play("r_up")
-		up = false
-	
-	if Input.is_action_pressed("ui_down") and down == false:
-		$AnimationPlayer.play("down")
-		down = true
-		
-	if not Input.is_action_pressed("ui_down") and down == true:
-		$AnimationPlayer.play("r_down")
-		down = false
-		
-	if Input.is_action_pressed("ui_left") and left == false:
-		$AnimationPlayer.play("left")
-		left = true
-	
-	if not Input.is_action_pressed("ui_left") and left == true:
-		$AnimationPlayer.play("r_left")
-		left = false
-		
-	if Input.is_action_pressed("ui_right") and right == false:
-		$AnimationPlayer.play("right")
-		right = true
-	
-	if not Input.is_action_pressed("ui_right") and right == true:
-		$AnimationPlayer.play("r_right")
-		right = false
 
+func _anim(direction, direction_name, opposite_direction_name,player):
+	var animation_name = direction_name 
+	var r_animation_name = "r_"+direction_name
+	var opposite_animation_name = opposite_direction_name 
+	var r_opposite_animation_name = "r_"+opposite_direction_name
+	
+	if direction == true and animation_done_status[animation_name] == false and animation_done_status[r_opposite_animation_name] == true:
+		if player.current_animation == "":
+			player.play(animation_name)
+			animation_done_status[r_opposite_animation_name] = false
+			animation_done_status[r_animation_name] = false
+			animation_done_status[opposite_animation_name] = false
+		
+	if direction == false and animation_done_status[animation_name] == true:
+		player.play(r_animation_name)
+		animation_done_status[animation_name] = false
+		animation_done_status[r_opposite_animation_name] = false
+		animation_done_status[opposite_animation_name] = false
+
+func _on_AnimationPlayer_animation_finished(animation_name):
+	animation_done_status[animation_name] = true
+
+
+func _on_AnimationPlayer2_animation_finished(animation_name):
+	animation_done_status[animation_name] = true
