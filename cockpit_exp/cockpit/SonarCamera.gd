06e54692a27c5b10b4ee9553a5e78f7c7d595d3d
nealith <nealith@neaworld.fr>
2019-01-31 10:31:15 +0100

    improve sonar rotation

--- cockpit_exp/cockpit/SonarCamera.gd
+++ cockpit_exp/cockpit/SonarCamera.gd
@@ -17,12 +17,22 @@ func _process(delta):
 	# Called every frame. Delta is time since last frame.
 	# Update game logic here.
 	global_transform.origin = get_node(target).global_transform.origin + tra
-	global_transform.basis = get_node(target).global_transform.basis
+	var _rot = get_node(target).global_transform.basis.get_euler()
 	
 	var rot = global_transform.basis.get_euler()
 	
-	global_rotate(Vector3(1,0,0),ori_rot.x - rot.x)
-	global_rotate(Vector3(0,0,1),ori_rot.z - rot.z)
+	var r = Vector3(ori_rot.x - rot.x,_rot.y-rot.y,ori_rot.z - rot.z)
+	
+	
+	if r.x < -0.01 or r.x > 0.01:
+		rotate_x(r.x)
+	if r.z < -0.01 or r.z > 0.01:
+		rotate_z(r.z)
+	if r.y < -0.01 or r.y > 0.01:
+		rotate_y(r.y)
+	
+	
+	
 
 	
 	
