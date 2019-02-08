11033b3d84d3f8b76c6055290d11f56eeba9544c
nealith <nealith@neaworld.fr>
2019-02-08 13:11:32 +0100

    continue sfx on dome

--- cockpit/dome/crack_generator.gd
+++ cockpit/dome/crack_generator.gd
@@ -4,21 +4,29 @@ extends Spatial
 # var a = 2
 # var b = "textvar"
 
+var playing = true
+var extends_craking = false
+var distance = 0
+
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	
-	$CrackPass/TextureRect.material.set_shader_param('noise',$Noise.get_texture())
-	$CrackPass/TextureRect.material.set_shader_param('previous',$CrackPass.get_texture())
+	# $CrackPass/TextureRect.material.set_shader_param('previous',$CrackPass.get_texture())
+	pass
 
 func _process(delta):
 	# Called every frame. Delta is time since last frame.
 	# Update game logic here.
-	pass
-	#$CrackPass/TextureRect.material.set_shader_param('previous',$CrackPass.get_texture())
+	$Sprite3D.texture = $CrackPass.get_texture()
+	
 	
 func play():
 	pass
 	
 func reset():
 	pass
+
+func _on_Timer_timeout():
+	
+	distance += 1
+	$CrackPass/TextureRect.material.set_shader_param('distance_i',distance)
--- cockpit/dome/dome.gd
+++ cockpit/dome/dome.gd
@@ -7,10 +7,10 @@ extends Spatial
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	#$Sphere.material_override.depth_texture = $CrackGenerator/CrackPass.get_texture()
+	
 	pass
 
 func _process(delta):
 	# Called every frame. Delta is time since last frame.
 	# Update game logic here.
-	pass
+	$Sphere.material_override.albedo_texture = $CrackGenerator/CrackPass.get_texture()
