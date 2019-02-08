d006e144fe06e13ee555fd1149db7cf7482b7b3e
nealith <nealith@neaworld.fr>
2019-02-08 21:07:52 +0100

    continue dome sfx

--- cockpit/dome/dome.gd
+++ cockpit/dome/dome.gd
@@ -4,13 +4,28 @@ extends Spatial
 # var a = 2
 # var b = "textvar"
 
+var playing = true
+var distance = 0
+
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	
+	# $CrackPass/TextureRect.material.set_shader_param('previous',$CrackPass.get_texture())
 	pass
 
 func _process(delta):
 	# Called every frame. Delta is time since last frame.
 	# Update game logic here.
-	$Sphere.material_override.albedo_texture = $CrackGenerator/CrackPass.get_texture()
+	pass
+	
+	
+func play():
+	pass
+	
+func reset():
+	pass
+
+func _on_Timer_timeout():
+	
+	distance += 1
+	$Sphere.material_override.next_pass.set_shader_param('distance_i',distance)
