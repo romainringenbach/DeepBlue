6ba42fea186fbc0bb5ece4c8e8de01313c920484
fonspa <fonspa@gmail.com>
2019-02-10 18:15:46 +0100

    new models and terrain addon. Fingers crossed.

---
+++ Scripts/Followpath.gd
@@ -0,0 +1,18 @@
+extends Path
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+onready var path = get_node("PathFollow")
+var tween
+
+func _ready():
+	tween = Tween.new()
+	add_child(tween)
+	tween.interpolate_property(path, "unit_offset", 0, 1, 10, tween.TRANS_LINEAR, tween.EASE_IN_OUT)
+	tween.set_repeat(true)
+	tween.start()
+
+func _process(delta):
+	#path.set_offset(path.get_offset() + 1 * delta)
+	pass
---
+++ Scripts/Infos.gd
@@ -0,0 +1,7 @@
+extends Label
+
+func _ready():
+	pass
+
+func _process(delta):
+	text = "Draw calls: " + str(Performance.get_monitor(Performance.RENDER_DRAW_CALLS_IN_FRAME))
